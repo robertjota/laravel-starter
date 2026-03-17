572aac87e2f5c020da25dd6ffd28bdbb8752db8a
Robert Arias <robert.arias@suelatexocu.lan>
2026-03-17 11:41:28 -0400

    Upgrade Vite to v7.x (Laravel 12 default)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -11,14 +10,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                }
-            }
-        })
     ],
     resolve: {
         alias: {
