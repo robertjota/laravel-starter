c92ba317ea1ac05123300f9b5ce18fcc83507cb8
Robert Arias <robertjota@github.com>
2026-03-19 23:42:24 -0400

    feat: Super Admin has full access to all resources
    - Add before() method to UserPolicy, RolePolicy, PermissionPolicy
    - Super Admin role bypasses all permission checks
    - No need to assign individual permissions to Super Admin
    - Other users still require specific permissions

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,6 +9,14 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    public function before(User $user, string $ability): bool|null
+    {
+        if ($user->hasRole('Super Admin')) {
+            return true;
+        }
+        return null;
+    }
+
     public function viewAny(User $user): bool
     {
         return $user->hasPermissionTo('admin.users.index');
